2d7e3c2b3c511b39879faa532de5263b1c7c76f6
Xavier Lau <c.estlavie@icloud.com>
2019-07-10 21:09:09 +0800

    remove unused error

--- src/server/engine/hash/error.rs
+++ src/server/engine/hash/error.rs
@@ -1,14 +1,13 @@
-#[allow(dead_code)]
 #[derive(Debug, Fail)]
 pub enum HashEngineError {
-    #[fail(display = "RwLock Poison")]
-    RwLockPoisonError,
+//    #[fail(display = "RwLock Poison")]
+//    RwLockPoisonError,
     #[fail(display = "Key: `{:?}`, not found", k)]
     KeyNotFound { k: Vec<u8> },
     #[fail(display = "File: `{}`, not found", path)]
     FileNotFound { path: String },
     #[fail(display = "Merge locked")]
     MergeLocked,
-    #[fail(display = "Nothing to scan")]
-    EmptyScanMap,
+//    #[fail(display = "Nothing to scan")]
+//    EmptyScanMap,
 }
